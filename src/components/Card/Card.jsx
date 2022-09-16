f49f8d227a7632a16febe22ab8b2239229790954
Natalya <m_d.r.lecter@protonmail.com>
2022-09-17 02:25:03 +0400

    image & felicitation components

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,14 @@
 import style from './Card.module.css';
+import ImageCard from '../ImageCard/ImageCard';
+import Felicitation from '../Felicitation/Felicitation';
+import CardBG from '../../img/card-bg.jpg';
 
 const Card = () => (
     <div className={style.card}>
     <div className={style.wrapper}>
       <div className={style.image}>
-        <img className={style.img} src='' alt=''/>
-        <p className={style.felicitation}>
-          Поздравляю с днем рожденья!<br/>
-          Пусть будет жизнь полна веселья,<br/>
-          Не будет грусти и хлопот,<br/>
-          А только счастье круглый год!<br/>
-          <br/>
-          Желаю творческих успехов,<br/>
-          Прекрасных дней, улыбок, смеха.<br/>
-          Любви, душевного тепла,<br/>
-          Как сказка, чтобы жизнь была!<br/>
-          </p>
+        <ImageCard img={CardBG}/>
+        <Felicitation />
       </div>
     </div>
   </div>
