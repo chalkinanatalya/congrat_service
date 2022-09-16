3a7d14af63a5fd9193ced3e096baf3809105acc0
Natalya <m_d.r.lecter@protonmail.com>
2022-09-17 02:04:32 +0400

    card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Card from './components/Card/Card';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
@@ -7,30 +8,12 @@ const wrapper = {
   height: '100vh',
 }
 
-const style = {};
 
 const App = () => {
   return (
     <div style={wrapper}>
       <Header />
-      <div className={style.card}>
-        <div className={style.wrapper}>
-          <div className={style.image}>
-            <img className={style.img} src='' alt=''/>
-            <p className={style.felicitation}>
-              Поздравляю с днем рожденья!<br/>
-              Пусть будет жизнь полна веселья,<br/>
-              Не будет грусти и хлопот,<br/>
-              А только счастье круглый год!<br/>
-              <br/>
-              Желаю творческих успехов,<br/>
-              Прекрасных дней, улыбок, смеха.<br/>
-              Любви, душевного тепла,<br/>
-              Как сказка, чтобы жизнь была!<br/>
-              </p>
-          </div>
-        </div>
-      </div>
+      <Card />
       <Footer />
     </div>
   );
