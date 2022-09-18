600eaee154ff5dec069e2a500ca2e895a7003783
Natalya <m_d.r.lecter@protonmail.com>
2022-09-19 03:40:39 +0400

    custom hook and refactoring

--- src/components/Header/Choices/Choices.jsx
+++ src/components/Header/Choices/Choices.jsx
@@ -1,43 +1,34 @@
 import style from './Choices.module.css';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import { holidaysContext } from '../../../context/holidayContext';
-import { URI_API } from '../../../const/const.js';
 
 const Choices = () => {
     const [isOpenChoises, setIsOpenChoices] = useState(false);
-    const {holiday, setHoliday} = useContext(holidaysContext);
-    const [holidays, setHolidays] = useState({});
+    const {holidays, holiday, changeHoliday} = useContext(holidaysContext);
 
-    useEffect(() => {
-        fetch(URI_API)
-            .then(response => {
-                if(response.status !== 200) {
-                    throw new Error (response.status)
-                }
-                return response.json()
-            })
-            .then(data => setHolidays(data))
-            .catch(err => console.error(err));
-    }, [setHolidays]);
 
     const toggeChoises = () => {
         setIsOpenChoices(!isOpenChoises);
     };
-    const changeHoliday = (title) => {
-        setHoliday(title);
-        toggeChoises();
-    }
+
 return (
     <div className={style.wrapper}>
-        <button className={style.button} onClick={toggeChoises}>{holiday}</button>
+        <button className={style.button} onClick={toggeChoises}>
+            {holidays[holiday] || 'Выбрать праздник'}
+            </button>
     {isOpenChoises && (
         <ul className={style.list}>
         {Object.entries(holidays).map(item => (
             <li 
             className={style.item} 
             key={item[0]}
-            onClick={() => changeHoliday(item[1])}
-            >{item[1]}</li>
+            onClick={() => {
+                changeHoliday(item[0]);
+                toggeChoises();
+                }
+            }
+            >{item[1]}
+            </li>
         ))}
         </ul>
     )
